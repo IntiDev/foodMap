26455f7f4e0689bb252f450953dca3fe9f71353f
MemeCast <memecast@MacBook-de-Jonathan.local>
2017-06-05 12:20:26 -0500

    maquetacion inicial completada, creando repositorio

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-var cargarPagina = function (){
-  $("#get-geolocation").click(obtenerUbicacion);
-};
-var obtenerUbicacion = function(){
-  console.log("hey", navigator.geolocation.getCurrentPosition(mostrarUbicacion));
-}
-$(document).ready(cargarPagina);
